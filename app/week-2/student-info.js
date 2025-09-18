f0825683b90538ee60becb8001a7fa3d3ff63a84
monic <moniclee421@gmail.com>
2025-09-17 23:57:14 -0600

    Week-2: add page and StudentInfo; update root page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <section>
+      <p>Name: Dominique Anne Lee</p>
+      <p>
+        GitHub: <Link href="https://github.com/monic421/cprg306-assignments" target="_blank"><u>monic421/cprg306-assignments</u></Link>
+      </p>
+    </section>
+  );
+}
