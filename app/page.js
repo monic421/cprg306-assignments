4dadb698a7fe99ea019c918e7fc0f3f8911ca2d0
monic <moniclee421@gmail.com>
2025-09-22 11:47:02 -0600

    week-2 add page and StudentInfo; update root page

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p><Link href="/week-2">Go to Week 2</Link></p>
+      <p><Link href="/week-2"> Go to Week 2 ➜</Link></p>
     </main>
   );
 }
