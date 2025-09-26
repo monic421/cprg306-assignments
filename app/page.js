987754d531a07a58fdefdbfb6424d9ba74559d3c
monic <moniclee421@gmail.com>
2025-09-26 01:11:39 -0600

    week-3: add page, Item, ItemList + link

--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p><Link href="/week-2"> Go to Week 2 ➜</Link></p>
-      <p><Link href="/week-3"> Go to Week 3 ➜</Link></p>
+      <p>
+        <Link href="/week-2"> Go to Week 2 ➜</Link>
+        <Link href="/week-3"> Go to Week 3 ➜</Link>
+      </p>
     </main>
   );
 }
